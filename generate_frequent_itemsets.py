e45a30b4a54ea8df96dc698e05c67d235cd1b53f
reesekshub <eer.11.reese@gmail.com>
2026-04-29 15:39:56 -0600

    finished generating association rules

--- generate_frequent_itemsets.py
+++ generate_frequent_itemsets.py
@@ -1,4 +1,5 @@
 import load_data as ld
+from itertools import combinations
 import sklearn.cluster as cluster
 import sklearn.mixture as mixture
 
@@ -36,14 +37,45 @@ def generate_kmeans_clusters():
     # add clusters in city list form to cluster_labels
     for i in range(n_unlabeled_diff_clusters):
         cluster_labels.append(unlabeled_diff_df.index[km.labels_ == i].tolist())
-        
+
     return cluster_labels
 
 # compute GMM Clusters
+def generate_gmm_clusters() -> list:
+    cluster_labels = [] # list of clusters, each of which is a list of cities in that cluster
+    # diff clusters, n_components = 14, determined by AIC/BIC in gmm_cluster.ipynb
+    n_diff_clusters = 14
+    gmm = mixture.GaussianMixture(n_components=n_diff_clusters, random_state=42, reg_covar = 1e-3)
+    gmm.fit(diff_df)
+    # add clusters in city list form to cluster_labels
+    for i in range(n_diff_clusters):
+        cluster_labels.append(diff_df.index[gmm.predict(diff_df) == i].tolist())
+
+    # ratio clusters, n_components = 10
+    n_ratio_clusters = 10
+    gmm = mixture.GaussianMixture(n_components=n_ratio_clusters, random_state=42, reg_covar = 1e-5)
+    gmm.fit(ratio_df)
+    # add clusters in city list form to cluster_labels
+    for i in range(n_ratio_clusters):
+        cluster_labels.append(ratio_df.index[gmm.predict(ratio_df) == i].tolist())
+
+    # unlabeled diff clusters, n_components = 8
+    n_unlabeled_diff_clusters = 8
+    gmm = mixture.GaussianMixture(n_components=n_unlabeled_diff_clusters, random_state=42, reg_covar = 1e-3)
+    gmm.fit(unlabeled_diff_df)
+    # add clusters in city list form to cluster_labels
+    for i in range(n_unlabeled_diff_clusters):
+        cluster_labels.append(unlabeled_diff_df.index[gmm.predict(unlabeled_diff_df) == i].tolist())
+
+    return cluster_labels
 
 
-
-# Generate frequent itemsets of size n from a cluster c
+# Generate frequent itemsets of size n from a cluster
+# basically just combinations of cluster
+# if this is super slow I'll revisit it
+def generate_frequent_itemsets(cluster, n):
+    itemsets = [list(comb) for comb in combinations(cluster, n)]
+    return itemsets
 
 
 
